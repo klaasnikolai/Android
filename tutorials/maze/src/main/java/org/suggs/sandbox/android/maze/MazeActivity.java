4efa0124aa80b4e09510d074f774e41142078fc3
Peter Suggitt <me@suggs.org.uk>
2011-09-26 07:28:58 +0100

    Created initial project for tilting maze

---
+++ tutorials/maze/src/main/java/org/suggs/sandbox/android/maze/MazeActivity.java
@@ -0,0 +1,15 @@
+package org.suggs.sandbox.android.maze;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MazeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
