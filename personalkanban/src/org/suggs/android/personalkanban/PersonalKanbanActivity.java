3e8913dba8655685d1f6de74929daa6f26454189
Peter Suggitt <me@suggs.org.uk>
2011-09-30 07:12:33 +0100

    Added personal kanban project

---
+++ personalkanban/src/org/suggs/android/personalkanban/PersonalKanbanActivity.java
@@ -0,0 +1,15 @@
+package org.suggs.android.personalkanban;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PersonalKanbanActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
