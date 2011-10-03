b128165508c9c708e03b542476e1c9164683e43b
Peter Suggitt <me@suggs.org.uk>
2011-10-03 20:05:14 +0100

    Added all basic layout tutorial projects

---
+++ tutorials/layouts/listview/src/org/suggs/sandbox/android/layouts/listview/ListViewActivity.java
@@ -0,0 +1,15 @@
+package org.suggs.sandbox.android.layouts.listview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListViewActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ tutorials/layouts/relative/src/org/suggs/sandbox/android/layouts/relative/RelativeActivity.java
@@ -0,0 +1,15 @@
+package org.suggs.sandbox.android.layouts.relative;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RelativeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
